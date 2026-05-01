a46498384e7430795bf171bf1ed2f00fb494452e
BobKaLonger <rkollingeriii@gmail.com>
2026-05-01 08:45:08 -0400

    initial version

---
+++ ModEntry.cs
@@ -0,0 +1,14 @@
+using System;
+using HarmonyLib;
+using StardewModdingAPI;
+using StartdewModdingAPI.Events;
+using System.Collections.Generic;
+using StardewValley;
+
+namespace UnlimitedIncubatorHatches
+{
+    public class ModEntry : Mod
+    {
+        
+    }
+}
