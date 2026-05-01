88e991a44ad11fb080e4a9e9563e7a700053e396
BobKaLonger <rkollingeriii@gmail.com>
2026-05-01 10:44:05 -0400

    initial code

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,14 +1,23 @@
-using System;
-using HarmonyLib;
-using StardewModdingAPI;
-using StartdewModdingAPI.Events;
-using System.Collections.Generic;
-using StardewValley;
-
 namespace UnlimitedIncubatorHatches
 {
     public class ModEntry : Mod
     {
-        
+        internal static Queue<AnimalHouse> PendingHatches = new();
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += (s, e) => PendingHatches.Clear();
+
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(AnimalHouse), "resetSharedState"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.ResetSharedState_Postfix))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(AnimalHouse), nameof(AnimalHouse.addNewHatchedAnimal)),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.AddNewHatchedAnimal_Postfix))
+            );
+        }
     }
 }
---
+++ Patches.cs
@@ -0,0 +1,60 @@
+namespace UnlimitedIncubatorHatches
+{
+    internal class Patches
+    {
+        public static void ResetSharedState_Postfix(AnimalHouse __instance)
+        {
+            int found = 0;
+            foreach (Object o in __instance.objects.Values)
+            {
+                if (!o.bigCraftable) continue;
+                var machineData = o.GetMachineData();
+                if (machineData == null || !machineData.IsIncubator
+                    || o.heldObject.Value == null || o.MinutesUntilReady > 0) continue;
+                
+                found++;
+                if (found == 1) continue;
+                ModEntry.PendingHatches.Enqueue(__instance);
+            }
+        }
+
+        public static void AddNewHatchedAnimal_Postfix()
+        {
+            if (ModEntry.PendingHatches.Count == 0) return;
+
+            AnimalHouse house = ModEntry.PendingHatches.Dequeue();
+
+            if (house.isFull())
+            {
+                if (!house.hasShownIncubatorBuildingFullMessage)
+                {
+                    house.hasShownIncubatorBuildingFullMessage = true;
+                    Game1.showGlobalMessage(
+                        Game1.content.LoadString("Strings\\Locations:AnimalHouse_Incubator_HouseFull"));
+                }
+                ModEntry.PendingHatches.Clear();
+                return;
+            }
+
+            string whatHatched = "??";
+            foreach (Object o in house.objects.Values)
+            {
+                if (!o.bigCraftable) continue;
+                var machineData = o.GetMachineData();
+                if (machineData == null || !machineData.IsIncubator
+                    || o.heldObject.Value == null || o.MinutesUntilReady > 0) continue;
+                
+                FarmAnimalData hatchedAnimal = FarmAnimal.GetAnimalDataFromEgg(o.heldObject.Value, house);
+                if (hatchedAnimal?.BirthText != null)
+                    whatHatched = TokenParser.ParseText(hatchedAnimal.BirthText);
+                break;
+            }
+
+            house.currentEvent = new EventArgs(
+                "none/-1000 -1000/farmer 2 9 0/pause 250/message \""
+                + whatHatched
+                + "\"/pause 500/animalNaming/pause 500/end"
+            );
+        }
+    }
+}
